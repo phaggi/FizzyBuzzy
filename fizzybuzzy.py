9f3db9e0d2504855f906eaed4eebde707aaabe4d
phaggi <phaggi@gmail.com>
2021-05-21 00:32:15 +0300

    make all variables unical

--- fizzybuzzy.py
+++ fizzybuzzy.py
@@ -1,18 +1,18 @@
-def fb(i, number, fbdict):
-    return str(fbdict[i % number][number])
+def fb(i, number_, fb_dict_):
+    return str(fb_dict_[i % number_][number_])
 
 
-def fbcalc(size, numbers, fbdict):
-    a, b = numbers
+def fb_calc(size, _numbers, _fb_dict):
+    a, b = _numbers
     for i in range(size):
-        print(''.join({fb(i, a, fbdict), fb(i, b, fbdict)}) or i)
+        print(''.join({fb(i, a, _fb_dict), fb(i, b, _fb_dict)}) or i)
 
 
 if __name__ == '__main__':
     numbers = (3, 13)
-    fbsize = 1000000
-    nonfb = {numbers[0]: '', numbers[1]: ''}
-    fbdict = [{numbers[0]: 'fizz', numbers[1]: 'buzz'}]
+    fb_size = 1000000
+    non_fb = {numbers[0]: '', numbers[1]: ''}
+    fb_dict = [{numbers[0]: 'fizz', numbers[1]: 'buzz'}]
     for j in range(max(numbers)):
-        fbdict.append(nonfb)
-    fbcalc(fbsize, numbers, fbdict)
+        fb_dict.append(non_fb)
+    fb_calc(fb_size, numbers, fb_dict)
