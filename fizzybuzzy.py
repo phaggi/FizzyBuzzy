d58b3fdf1d7c32ee7bf8ce42d49a7b73eda960c0
phaggi <phaggi@gmail.com>
2021-05-21 00:24:27 +0300

    remove global variables

--- fizzybuzzy.py
+++ fizzybuzzy.py
@@ -1,18 +1,19 @@
-def fb(i, number):
+def fb(i, number, fbdict):
     return str(fbdict[i % number][number])
 
-def fbcalc(size, numbers):
+
+def fbcalc(size, numbers, fbdict):
     a, b = numbers
     for i in range(size):
-        print(''.join(set([fb(i, a)
-        , fb(i, b)])) or i)
+        print(''.join(set([fb(i, a, fbdict)
+                              , fb(i, b, fbdict)])) or i)
 
 
 if __name__ == '__main__':
     numbers = (3, 13)
-    fbsize = 1000
+    fbsize = 1000000
     nonfb = {numbers[0]: '', numbers[1]: ''}
     fbdict = [{numbers[0]: 'fizz', numbers[1]: 'buzz'}]
     for j in range(max(numbers)):
         fbdict.append(nonfb)
-    fbcalc(fbsize, numbers)
+    fbcalc(fbsize, numbers, fbdict)
